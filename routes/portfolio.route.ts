b4a48790775747ac983d84435dc4bf94afb43c57
ImJustNon <Im_Just_Non@hotmail.com>
2024-12-02 09:55:47 +0700

    Update Popular to Post

--- routes/portfolio.route.ts
+++ routes/portfolio.route.ts
@@ -3,7 +3,7 @@ import { portfolioPopularController } from "../controllers/port_popular.controll
 import express, { Router } from "express";
 const router: Router = express.Router();
 
-router.post("/v1/portfolio/popular", portfolioPopularController);
+router.get("/v1/portfolio/popular", portfolioPopularController);
 router.get("/v1/portfolio/details/:id", portfolioDetailsController);
 
 export default router;
